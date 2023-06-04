5e29f8ec3ba3b6cfdbe41b4844f720c334a03beb
MATEBOOK-13\jared <jared0192galicia@gmail.com>
2023-06-04 05:16:51 -0600

    Initial Commit

---
+++ script.js
@@ -0,0 +1,96 @@
+
+/**
+ * 0 -> Roca
+ * 1 -> papel
+ * 2 -> Tijeras
+ */
+
+var selectionMachine;
+var ptBoot = 0;
+var ptPlayer = 0;
+
+var ptMax = prompt("Hasta cuantos puntos decea jugar?");
+
+function run() {
+
+    btnRock.onclick = selectedRock;
+    btnPaper.onclick = selectedPaper;
+    btnHand.onclick = selectedHand;
+
+}
+
+function selectedRock() {
+    selectOption();
+    winner(0);
+}
+function selectedPaper() {
+    selectOption();
+    winner(1);
+}
+function selectedHand() {
+    selectOption();
+    winner(2);
+}
+
+function winner(selec) {
+    if (selec == selectionMachine) {
+        alert('Empate');
+    } else if (selec == 0 && selectionMachine == 1) {
+        // alert('Perdiste');
+        ptBoot++;
+        countBoot.innerHTML = ptBoot; 
+
+    } else if (selec == 1 && selectionMachine == 0) {
+        // alert('Ganaste');
+        ptPlayer++;
+        countPlayer.innerHTML = ptPlayer; 
+
+    } else if (selec == 0 && selectionMachine == 2) {
+        // alert('Ganaste');
+        ptPlayer++;
+        countPlayer.innerHTML = ptPlayer;
+
+    } else if (selec == 2 && selectionMachine == 0) {
+        // alert('Perdiste');
+        ptBoot++;
+        countBoot.innerHTML = ptBoot; 
+
+    } else if (selec == 1 && selectionMachine == 2) {
+        // alert('Perdiste');
+        ptBoot++;
+        countBoot.innerHTML = ptBoot; 
+
+    } else if (selec == 2 && selectionMachine == 1) {
+        // alert('Ganaste');
+        ptPlayer++;
+        countPlayer.innerHTML = ptPlayer;
+
+    } else {
+        alert('Sin definir');
+    }
+
+    if (ptBoot == ptMax) {
+        alert("Haz Perdido!")
+    } else if (ptPlayer == ptMax) {
+        alert("Haz Ganado!!")
+    }
+}
+
+function selectOption() {
+    let actualClass = "fa-hand-back-fists";
+    let clases = ["fa-hand-back-fist", "fa-hand", "fa-hand-peace"];
+
+    // for (let i = 0; i < 3; i++) {
+    // machineSelection.classList.remove(actualClass);
+    selectionMachine = Math.floor(Math.random() * 3);
+
+    machineSelection.removeAttribute("class");
+    machineSelection.classList.add("fa-solid");
+
+    actualClass = clases[selectionMachine];
+    machineSelection.classList.add(actualClass);
+    console.log(machineSelection.className)
+    // }
+}
+
+run();
